92a5f1e542d3e4916c9f851a985b8f08a945ccba
Jake <jake.savins@student.torrens.edu.au>
2025-07-05 19:39:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("mobile-menu-btn");
+  const navList = document.querySelector("nav ul");
+
+   if (menuBtn && navList) {
+  menuBtn.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+}
+});
+
+document.getElementById("userForm").addEventListener("submit",function(event) {
+event.preventDefault();
+  
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+ if (!emailPattern.test(email)) {
+    alert("Please fill in all fields");
+  return;
+}
+alert(`Thank you, ${name}! Your email (${email}) has been
+received.`);
+document.getElementById('userForm').reset();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeElements = document.querySelectorAll(".fade-in");
+
+  fadeElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("visible");
+    }, index * 200); // 
+  });
+});
+
